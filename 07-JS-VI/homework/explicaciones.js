35738282d2c2a65ce60c85ff2f2bcdef75b774c4
Maxi <maxinz@gmail.com>
2022-10-19 21:13:56 -0300

    07-JS-VI homework DONE!

---
+++ 07-JS-VI/homework/explicaciones.js
@@ -0,0 +1,3 @@
+* Funciones Callback
+
+Se llama Callback al argumento de una función que recibe funciones como parámetro y las cuales son ejecutadas dentro de estas funciones que son capaces de importarlas.
